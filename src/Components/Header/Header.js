9e4df53b8b5abfed6aba7bffd1163644e4944b74
joelwaiman <waimanjoel@gmail.com>
2023-07-24 01:00:03 -0300

    header mobile

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,7 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import SearchIcon from '@mui/icons-material/Search';
 import { Context } from "../../CustomContext";
+import './header.css'
 
 const Header = () => {
 
@@ -29,24 +30,24 @@ const Header = () => {
     }
       
     return (
-        <header style={styles.header}>
-            <div style={styles.container1}>
+        <header className="header">
+            <div className= 'containerInput'>
                 <Link to="/">
-                    <img style={styles.img} alt={"logo"} src={mlpng} />
+                    <img className="img" alt={"logo"} src={mlpng} />
                 </Link>
-                <input value={inputValue} onChange={catchInput} placeholder="Que estas buscando?" style={styles.input} />
-                <button style={styles.button} onClick={search} >
+                <input value={inputValue} onChange={catchInput} placeholder="Que estas buscando?" className="input" />
+                <button className="button" onClick={search} >
                     <SearchIcon />
                 </button>
             </div>
-            <div style={styles.container2}>
-                <div style={styles.location}>
+            <div className="containerCategory">
+                <div className="location">
                     <LocationOnIcon />
                     <p>Enviar a Konoha</p>
                 </div>
                 <nav>
                     {categorys.map((category) => {
-                        return <NavLink style={styles.categorys} key={category.id} to={category.route}>{category.title}</NavLink>
+                        return <NavLink className='categories' key={category.id} to={category.route}>{category.title}</NavLink>
                     })}
                 </nav>
                 <ShoppingCartIcon />
@@ -56,66 +57,3 @@ const Header = () => {
 }
 
 export default Header;
-
-const styles = {
-    header: {
-        display: 'flex',
-        flexDirection: 'column',
-        backgroundColor: '#fff159',
-        fontFamily: 'roboto'
-    },
-    container1: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        maxWidth: 950,
-        margin: 'auto'
-    },
-    img: {
-        maxWidth: '2.5rem',
-        margin: '0 50px 0 0'
-    },
-    input: {
-        margin: '10px -5px 2px 0',
-        padding: '0 5px',
-        width: 600,
-        height: 40,
-        backgroundColor: '#FFFFFF',
-        border: 'transparent',
-        borderRadius: 4,
-    },
-    button: {
-        display: 'flex',
-        width: 46,
-        height: 39,
-        margin: '10px 0 2px 0',
-        backgroundColor: '#FFFFFF',
-        padding: '20px 5px',
-        border: 'transparent',
-        borderRadius: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#adadad',
-        borderLeft: '1px solid #e6e6e6'
-    },
-    container2: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        maxWidth: 950,
-        margin: '10px 0 10px 380px',
-        color: '#3339'
-    },
-    location: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    categorys: {
-        textDecoration: 'none',
-        color: '#3339',
-        margin: '0 10px'
-    },
-}
